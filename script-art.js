70755d6a447ff46ed6b279a71c6ccef636685e82
Pablo Selin <pabloselin@gmail.com>
2022-09-28 14:38:15 +0200

    functional gandi hosted solution

--- script-art.js
+++ script-art.js
@@ -2,44 +2,44 @@ console.log(data);
 
 // change range to load
 //data.responses = data.responses.slice(5000,5010);
-data.responses = data.responses.slice(5000,6500);
+data.responses = data.responses.slice(5000, 6500);
 
-
-
-let $grid = document.querySelector('.grid');
-let $overlay = document.querySelector('.loading');
-let $loading = document.querySelector('.loading p');
-let $labelFilters = document.querySelector('.filters--labels select');
-let $objectFilters = document.querySelector('.filters--objects select');
-let $wordFilters = document.querySelector('.filters--words input');
+let $grid = document.querySelector(".grid");
+let $overlay = document.querySelector(".loading");
+let $loading = document.querySelector(".loading p");
+let $labelFilters = document.querySelector(".filters--labels select");
+let $objectFilters = document.querySelector(".filters--objects select");
+let $wordFilters = document.querySelector(".filters--words input");
 let COUNT = 0;
 
 let brokenUrls = [];
 
 let unescapeString = (str) => {
-  return str.replace(/-/gi, ' ').replace(/\+/gi, '&');
-}
+  return str.replace(/-/gi, " ").replace(/\+/gi, "&");
+};
 
 let escapeString = (str) => {
-  return str.trim().replace(/ /gi, '-').replace(/&/gi, '+').toLowerCase();
-}
+  return str.trim().replace(/ /gi, "-").replace(/&/gi, "+").toLowerCase();
+};
 
 let loadImg = (url, wrapper, i) => {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     let img = new Image();
-    img.addEventListener('load', e => {
-      $loading.textContent = `${++COUNT} of ${data.responses.length} images loaded`;
-      resolve({img: img, wrapper: wrapper})
+    img.addEventListener("load", (e) => {
+      $loading.textContent = `${++COUNT} of ${
+        data.responses.length
+      } images loaded`;
+      resolve({ img: img, wrapper: wrapper });
     });
-    img.addEventListener('error', (e) => {
+    img.addEventListener("error", (e) => {
       brokenUrls.push(url);
       reject(new Error(`Failed to load image's URL: ${url}`));
-    })
-    setTimeout( () => {
+    });
+    setTimeout(() => {
       img.src = url;
-    },i*10);
-  })
-}
+    }, i * 10);
+  });
+};
 
 let loadDom = () => {
   let labels = new Set();
@@ -48,105 +48,115 @@ let loadDom = () => {
   let imagePromises = [];
 
   // ~~~~~~~~~~~~ GET DATA ~~~~~~~~~~~~
-  data.responses.forEach( (d,i) => {
+  data.responses.forEach((d, i) => {
     let wrapper = document.createElement("div");
     let localLables = [];
     let localObjects = [];
     let localWords = [];
     wrapper.classList.add("item");
 
-    d.labels.forEach( a => { labels.add(a); localLables.push(a); } );
-    d.objects.forEach( a => { objects.add(a); localObjects.push(a); } );
-    d.words.split(/[\n\r\s]/).forEach( a => { words.add(a); localWords.push(a); } );
+    d.labels.forEach((a) => {
+      labels.add(a);
+      localLables.push(a);
+    });
+    d.objects.forEach((a) => {
+      objects.add(a);
+      localObjects.push(a);
+    });
+    d.words.split(/[\n\r\s]/).forEach((a) => {
+      words.add(a);
+      localWords.push(a);
+    });
 
-    wrapper.setAttribute('data-labels', Array.from(localLables).join(' '));
-    wrapper.setAttribute('data-objects', Array.from(localObjects).join(' '));
-    wrapper.setAttribute('data-words', Array.from(localWords).join(' '));
+    wrapper.setAttribute("data-labels", Array.from(localLables).join(" "));
+    wrapper.setAttribute("data-objects", Array.from(localObjects).join(" "));
+    wrapper.setAttribute("data-words", Array.from(localWords).join(" "));
 
     let url = d.url;
     let imgp = loadImg(url, wrapper, i);
     imagePromises.push(imgp);
   });
 
-
-
   // ~~~~~~~~~~~~ LOAD LABEL DROPDOWN ~~~~~~~~~~~~
-  let op = document.createElement('option');
-  op.value = '*';
-  op.textContent = 'All';
+  let op = document.createElement("option");
+  op.value = "*";
+  op.textContent = "All";
   $labelFilters.appendChild(op);
 
-  labels.forEach( f => {
-    let op = document.createElement('option');
+  labels.forEach((f) => {
+    let op = document.createElement("option");
     op.value = `${f}`;
     op.textContent = f;
     $labelFilters.appendChild(op);
-  })
-
+  });
 
   // ~~~~~~~~~~~~ LOAD OBJECT DROPDOWN ~~~~~~~~~~~~
-  op = document.createElement('option');
-  op.value = '*';
-  op.textContent = 'All';
+  op = document.createElement("option");
+  op.value = "*";
+  op.textContent = "All";
   $objectFilters.appendChild(op);
 
-  objects.forEach( f => {
-    let op = document.createElement('option');
+  objects.forEach((f) => {
+    let op = document.createElement("option");
     op.value = `${f}`;
     op.textContent = f;
     $objectFilters.appendChild(op);
-  })
-
-
+  });
 
   // ~~~~~~~~~~~~ LOAD ALL IMAGES AND INIT ISO ~~~~~~~~~~~~
-  Promise.allSettled(imagePromises).then( (results) => {
-    let res = results.filter( r => r.status === "fulfilled" );
+  Promise.allSettled(imagePromises).then((results) => {
+    let res = results.filter((r) => r.status === "fulfilled");
 
-    res.forEach( (r) => {
+    res.forEach((r) => {
       r.value.wrapper.appendChild(r.value.img);
       $grid.appendChild(r.value.wrapper);
-    })
-
-    let iso = new Isotope( '.grid', {
-      itemSelector: '.item',
-      layoutMode: 'masonry'
     });
 
-    $labelFilters.addEventListener( 'change', function(e) {
+    let iso = new Isotope(".grid", {
+      itemSelector: ".item",
+      layoutMode: "masonry",
+    });
+
+    $labelFilters.addEventListener("change", function (e) {
       $objectFilters.options[0].selected = true;
       iso.arrange({
-        filter: function(item) {
-          return $labelFilters.value === "*" || item.dataset.labels.includes($labelFilters.value);
-        }
+        filter: function (item) {
+          return (
+            $labelFilters.value === "*" ||
+            item.dataset.labels.includes($labelFilters.value)
+          );
+        },
       });
     });
 
-    $objectFilters.addEventListener( 'change', function(e) {
+    $objectFilters.addEventListener("change", function (e) {
       $labelFilters.options[0].selected = true;
       iso.arrange({
-        filter: function(item) {
-          return $objectFilters.value === "*" || item.dataset.objects.includes($objectFilters.value);
-        }
+        filter: function (item) {
+          return (
+            $objectFilters.value === "*" ||
+            item.dataset.objects.includes($objectFilters.value)
+          );
+        },
       });
     });
 
-    $wordFilters.addEventListener( 'input', function(e) {
+    $wordFilters.addEventListener("input", function (e) {
       $labelFilters.options[0].selected = true;
       $objectFilters.options[0].selected = true;
       iso.arrange({
-        filter: function(item) {
+        filter: function (item) {
           let r = new RegExp($wordFilters.value, "gi");
           return r.test(item.dataset.words);
-        }
+        },
       });
     });
 
-
-    document.querySelectorAll('.-hidden').forEach( x => x.classList.remove('-hidden') );
-    $overlay.classList.add('-hidden');
-  })
-}
-
+    document
+      .querySelectorAll(".-hidden")
+      .forEach((x) => x.classList.remove("-hidden"));
+    $overlay.classList.add("-hidden");
+  });
+};
 
 loadDom();
